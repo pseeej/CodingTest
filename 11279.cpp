dc577b26aaf4d00414f09e38fed3dec686b8d6f3
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-02-13 16:53:09 +0900

    Primary Queue - 최대 힙
    +) Primary Queue 이용
    
    널리 잘 알려진 자료구조 중 최대 힙이 있다. 최대 힙을 이용하여 다음과 같은 연산을 지원하는 프로그램을 작성하시오.
    1. 배열에 자연수 x를 넣는다.
    2. 배열에서 가장 큰 값을 출력하고, 그 값을 배열에서 제거한다.
    프로그램은 처음에 비어있는 배열에서 시작하게 된다.

---
+++ 11279.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	priority_queue<int> pq;
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		if (temp == 0) {
+			if (pq.empty())
+				cout << "0\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+			pq.push(temp);
+	}
+
+
+
+
+	return 0;
+}
