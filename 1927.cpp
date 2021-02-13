c3ebed5a2a34e9b492fea3e89fdb79d9088cb38b
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-02-13 16:59:01 +0900

    Priority Queue - 최소 힙
    +) 우선순위 큐 이용. priority_queue<int, vector<int>, greater<int>> pq;로 작은 것부터 나열되는 큐 만듦
    
    널리 잘 알려진 자료구조 중 최소 힙이 있다. 최소 힙을 이용하여 다음과 같은 연산을 지원하는 프로그램을 작성하시오.
    1. 배열에 자연수 x를 넣는다.
    2. 배열에서 가장 작은 값을 출력하고, 그 값을 배열에서 제거한다.
    프로그램은 처음에 비어있는 배열에서 시작하게 된다.

---
+++ 1927.cpp
@@ -0,0 +1,33 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		if (temp == 0) {
+			if (pq.empty())
+				cout << "0\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+			pq.push(temp);
+	}
+
+
+	return 0;
+}
