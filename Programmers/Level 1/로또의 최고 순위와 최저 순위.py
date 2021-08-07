bb7bc9139ac5eb8f61954cd92a20da37e2ced9d3
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-08-07 15:38:15 +0900

    2021 Dev-Matching: 웹 백엔드 개발자(상반기) - 로또의 최고 순위와 최저 순위
    2nd trial

--- Programmers/Level 1/로또의 최고 순위와 최저 순위.py
+++ Programmers/Level 1/로또의 최고 순위와 최저 순위.py
@@ -1,24 +1,18 @@
+def getRank(rank):
+    rank_list = [6, 5, 4, 3, 2, 1]
+    
+    if rank != 0:
+        return rank_list.index(rank)+1
+    
+    return 6
+    
+
 def solution(lottos, win_nums):
     answer = []
     
     lowest = len(set(lottos) & set(win_nums))
     highest = lowest + lottos.count(0)
     
-    answer = [highest, lowest]
-    
-    for i in range(len(answer)):
-        rank = answer[i]
-        if rank == 6:
-            answer[i] = 1
-        elif rank == 5:
-            answer[i] = 2
-        elif rank == 4:
-            answer[i] = 3
-        elif rank == 3:
-            answer[i] = 4
-        elif rank == 2:
-            answer[i] = 5
-        else:
-            answer[i] = 6
+    answer = [getRank(highest), getRank(lowest)]
     
     return answer
