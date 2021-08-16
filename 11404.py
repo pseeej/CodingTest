63068849556767f41b2bc63b43f46c45100a9795
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-08-17 04:07:26 +0900

    Floyd-Warshall - 플로이드

---
+++ 11404.py
@@ -0,0 +1,22 @@
+N = int(input())
+K = int(input())
+
+arr = [[int(1e9) for i in range(N+1)] for i in range(N+1)]
+
+for i in range(K):
+    a, b, c = map(int, input().split())
+    arr[a][b] = min(arr[a][b], c)
+
+for i in range(N+1):
+    arr[i][i] = 0
+
+for k in range(1, N+1):
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            arr[i][j] = min(arr[i][j], arr[i][k]+arr[k][j])
+
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        print(arr[i][j], end=" ")
+    print()
