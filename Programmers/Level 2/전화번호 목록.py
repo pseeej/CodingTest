211df00580da9ea729719ee6a9b46f04b40ba573
Sejin Park <49185035+pseeej@users.noreply.github.com>
2023-11-27 18:08:29 +0900

    해시 - 전화번호 목록
    https://school.programmers.co.kr/learn/courses/30/lessons/42577
    
    역시 정답자들은 달라.... ㅠㅡㅠ

---
+++ Programmers/Level 2/전화번호 목록.py
@@ -0,0 +1,10 @@
+def solution(phone_book):
+    answer = True
+    
+    phone_book.sort()
+    
+    for i in range(len(phone_book)-1):
+        if len(phone_book[i]) < len(phone_book[i+1]) and phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            return False
+    
+    return answer
