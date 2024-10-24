7db176e77962602a318dfc475ba4c3347dd55dda
Sejin Park <49185035+pseeej@users.noreply.github.com>
2024-10-24 17:53:06 +0900

    문자열 - 나무 출력
    https://softeer.ai/practice/9655
    https://pseeej.tistory.com/74?category=1232628

---
+++ Softeer/나무 출력.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("           *********           \n");
+        sb.append("      *****         ****       \n");
+        sb.append("    **                  **     \n");
+        sb.append("   *                      *    \n");
+        sb.append(" **                        **  \n");
+        sb.append("*                            * \n");
+        sb.append("*                            * \n");
+        sb.append("**                          ** \n");
+        sb.append(" **  *    ****  ****    *  **  \n");
+        sb.append("   ** ***** ****** ***** **    \n");
+        sb.append("             ****              \n");
+        sb.append("             ****              \n");
+        sb.append("             ****              \n");
+        sb.append("             ****              \n");
+        sb.append("            ******             \n");
+        sb.append("           ********");
+
+        System.out.print(sb);
+    }
+}
